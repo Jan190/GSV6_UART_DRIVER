bb6a439cdcb2dce571d4ae52a505ef31a45ae1a2
Jan190 <jan.jakovljevic@gmail.com>
2021-11-01 20:16:48 +0100

    Pocetni commit header i source file

---
+++ N-H723ZG-GSV6_UART_DRIVER/Core/Inc/GSV6_UART_DRIVER.h
@@ -0,0 +1,13 @@
+/*
+ * GSV6_UART_DRIVER.h
+ *
+ *  Created on: Nov 1, 2021
+ *      Author: Jan
+ */
+
+#ifndef INC_GSV6_UART_DRIVER_H_
+#define INC_GSV6_UART_DRIVER_H_
+
+
+
+#endif /* INC_GSV6_UART_DRIVER_H_ */
---
+++ N-H723ZG-GSV6_UART_DRIVER/Core/Src/GSV6_UART_DRIVER.c
@@ -0,0 +1,8 @@
+/*
+ * GSV6_UART_DRIVER.c
+ *
+ *  Created on: Nov 1, 2021
+ *      Author: Jan
+ */
+
+
